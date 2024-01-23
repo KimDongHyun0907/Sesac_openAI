768025c6b8e92ad8b2e90743877c49e345dd86dd
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-01-23 15:18:27 +0900

    Add files via upload

--- HTML/flask_crawling_2/app.py
+++ HTML/flask_crawling_2/app.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    datas = [
+datas = [
         {'name': '반원', 'level': 60, 'point': 360, 'exp' : 45000},
         {'name': '반원2', 'level': 2, 'point': 20, 'exp' : 200},
         {'name': '반원3', 'level': 3, 'point': 30, 'exp' : 300}
     ]
+@app.route('/')
+def index():
     return render_template('index.html', datas = datas)
+
+@app.route('/index_table')
+def index_table():
+    return render_template('index_table.html', datas = datas)
 
 if __name__=="__main__":
     app.run(debug=True)
