cab007e88bc1f957f9f6d52dd24b1b6b2f6b5ccc
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-01-23 14:16:11 +0900

    Update app.py

--- HTML/flask_crawling_2/app.py
+++ HTML/flask_crawling_2/app.py
@@ -4,13 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user = '반원'
     datas = [
         {'name': '반원', 'level': 60, 'point': 360, 'exp' : 45000},
         {'name': '반원2', 'level': 2, 'point': 20, 'exp' : 200},
         {'name': '반원3', 'level': 3, 'point': 30, 'exp' : 300}
     ]
-    return render_template('index.html', user = user, datas = datas)
+    return render_template('index.html', datas = datas)
 
 if __name__=="__main__":
     app.run(debug=True)
