1460840de63afa9eeb47546ad0b257b61e16bcfe
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-02-05 11:19:12 +0900

    Add files via upload

---
+++ first_project/wck3b_v0.05/static/js/next_page_fail.js
@@ -0,0 +1,15 @@
+var video = document.getElementById("fullscreen-video");
+// Debugging: Log the video source to the console
+console.log("Video Source: ", video.src);
+
+// 비디오가 재생 완료되었을 때 다음 페이지로 이동
+video.addEventListener("ended", function() {
+    console.log("Video ended. Navigating to the next page.");
+    window.location.href = "wrong"; // 'final_page'는 최종 페이지의 라우트 이름으로 변경해야 합니다.
+});
+
+// 0.01초(10밀리초) 후에 비디오를 재생
+setTimeout(function() {
+    console.log("Starting video playback after 0.01 seconds.");
+    video.play();
+}, 10);
---
+++ first_project/wck3b_v0.05/static/js/predicting.js
@@ -0,0 +1,4 @@
+// 5초(5000밀리초)가 지난 후에 다음 페이지로 이동
+setTimeout(function() {
+    window.location.href = "keeperprediction"; // 'next_page'는 다음 페이지의 라우트 이름으로 변경해야 합니다.
+}, 5000);
