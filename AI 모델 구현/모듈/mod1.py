93e00578529a802fd7966c2e0b90d71a19fcfb92
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2024-02-26 22:36:00 +0900

    Add files via upload

---
+++ AI 모델 구현/모듈/mod1.py
@@ -0,0 +1,13 @@
+# https://wikidocs.net/29
+
+import sys, os
+
+def add(a,b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+if __name__ == "__main__":
+    print(add(1,4))
+    print(sub(4,2))
